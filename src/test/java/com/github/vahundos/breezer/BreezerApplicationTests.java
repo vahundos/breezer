14fe15f90b6c64a8861d4783a5b9a3c77cb563dc
Volodymyr Avdeev <avdeev.volodymyr@gmail.com>
2020-01-01 14:31:02 +0200

    using hsqldb for tests

--- src/test/java/com/github/vahundos/breezer/BreezerApplicationTests.java
+++ src/test/java/com/github/vahundos/breezer/BreezerApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.vahundos.breezer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BreezerApplicationTests {
 
 	@Test
