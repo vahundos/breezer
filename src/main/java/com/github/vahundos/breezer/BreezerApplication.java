70e48821db133545952c1ebc8d3da511ecdb2bf8
Volodymyr Avdeev <avdeev.volodymyr@gmail.com>
2020-01-01 12:15:12 +0200

    created project structure

---
+++ src/main/java/com/github/vahundos/breezer/BreezerApplication.java
@@ -0,0 +1,13 @@
+package com.github.vahundos.breezer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreezerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreezerApplication.class, args);
+	}
+
+}
