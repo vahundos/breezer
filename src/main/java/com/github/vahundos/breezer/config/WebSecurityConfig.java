08f3a668739674fa78048315ea5cad776b521014
Volodymyr Avdeev <avdeev.volodymyr@gmail.com>
2020-04-26 17:46:17 +0300

    added cors config

--- src/main/java/com/github/vahundos/breezer/config/WebSecurityConfig.java
+++ src/main/java/com/github/vahundos/breezer/config/WebSecurityConfig.java
@@ -4,12 +4,18 @@ import com.github.vahundos.breezer.web.filter.BasicAuthenticationDenyFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,8 +28,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         // @formatter:off
         http
+            .cors()
+                .and()
             .authorizeRequests()
-                .antMatchers("/", "/users/register").permitAll()
+                .antMatchers("/users/login", "/users/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -37,6 +45,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.addFilterBefore(basicAuthenticationDenyFilter, BasicAuthenticationFilter.class);
         // @formatter:on
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:8000"));
+        configuration.setAllowedMethods(List.of("GET", "POST"));
+        configuration.setAllowedHeaders(List.of(HttpHeaders.ACCEPT, HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION,
+                                                "X-Auth-Token"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
