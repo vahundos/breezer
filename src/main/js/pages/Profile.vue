0e444302be9d3f4302a798860a5e2f279ce902d5
Volodymyr Avdeev <avdeev.volodymyr@gmail.com>
2020-06-21 13:49:24 +0300

    updated profile layout

--- src/main/js/pages/Profile.vue
+++ src/main/js/pages/Profile.vue
@@ -1,6 +1,69 @@
 <template>
     <v-container>
-        <v-img :src="image" width="200" height="200"/>
+        <v-card>
+            <v-card-title>Your profile</v-card-title>
+            <v-card-text>Some information can be visible for other users</v-card-text>
+        </v-card>
+        <v-card>
+            <v-row class="align-center">
+                <v-col class="text-center">
+                    <v-card-text>
+                        Photo
+                    </v-card-text>
+                </v-col>
+                <v-col class="text-center">Photo to help recognize you</v-col>
+                <v-col class="text-center">
+                    <v-avatar size="64">
+                        <v-img :src="image"/>
+                    </v-avatar>
+                </v-col>
+            </v-row>
+        </v-card>
+        <v-card>
+            <v-row class="align-center">
+                <v-col class="text-center">
+                    <v-card-text>
+                        Name
+                    </v-card-text>
+                </v-col>
+                <v-col class="text-center">{{this.$store.state.user.firstName + ' ' + this.$store.state.user.secondName}}</v-col>
+                <v-col class="text-center">
+                    <v-btn v-if="this.$store.state.authToken" icon>
+                        <v-icon>keyboard_arrow_right</v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-card>
+        <v-card>
+            <v-row class="align-center">
+                <v-col class="text-center">
+                    <v-card-text>
+                        Email
+                    </v-card-text>
+                </v-col>
+                <v-col class="text-center">{{this.$store.state.user.email}}</v-col>
+                <v-col class="text-center">
+                    <v-btn v-if="this.$store.state.authToken" icon>
+                        <v-icon>keyboard_arrow_right</v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-card>
+        <v-card>
+            <v-row class="align-center">
+                <v-col class="text-center">
+                    <v-card-text>
+                        Password
+                    </v-card-text>
+                </v-col>
+                <v-col class="text-center">*******</v-col>
+                <v-col class="text-center">
+                    <v-btn v-if="this.$store.state.authToken" icon>
+                        <v-icon>keyboard_arrow_right</v-icon>
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-card>
     </v-container>
 </template>
 
